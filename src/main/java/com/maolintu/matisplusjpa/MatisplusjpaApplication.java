ad71f44a75fb46224fecfeaa82b577cd2a1dbd1e
tumaolin94 <524561223@qq.com>
2018-04-11 15:22:31 -0700

    Init

---
+++ src/main/java/com/maolintu/matisplusjpa/MatisplusjpaApplication.java
@@ -0,0 +1,12 @@
+package com.maolintu.matisplusjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MatisplusjpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MatisplusjpaApplication.class, args);
+    }
+}
